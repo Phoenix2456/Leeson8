5399103b8bda6774d8af9517e7c345660432a10e
Benny <phoenixshadow2456@gmail.com>
2024-10-15 22:30:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Create a function that determines if a string contains any digits and returns that information to the function call. 
+
+Summary: function that returns whether or not a string contains any digits.
+Input: a string (not from user)
+Edge case: Empty string
+Output: True/False
+
+Coding tools needed: return, print, for loop, if
+
+More info needed: How do we define the condition of being a digit? .isdigit()
+"""
+
+
+def contains_digits(a_string):
+    pass
+
+
+"""
+def contains_digits(a_string): <---sample code given
+
+  FOR character in string
+    IF character = a digit
+      RETURN True
+  RETURN False
+
+function call var = function call with string argument
+PRINT function call var
+
+
+"""
